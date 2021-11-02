f7402cbd396204036afd791e87371a31e5b7ba88
Janet Iqal <Janet@MacBook-Air-2.local>
2021-11-02 16:03:43 -0500

    added navbar and started a theme

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,24 @@
-
-
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./Components/NavigationBar";
+import { CssBaseline } from "@material-ui/core";
+import { ThemeProvider } from "@material-ui/styles";
+import  theme  from "./theme";
 function App() {
   return (
     <div>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route path="/" exact />
+            <Route path="/work" exact />
+            <Route path="/resume" exact />
+            <Route path="/contact" exact />
+          </Switch>
+        </Router>
+    </ThemeProvider>
     </div>
   );
 }
---
+++ portfolio/src/Components/NavigationBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  makeStyles,
+  Button
+} from "@material-ui/core";
+
+import DrawerComponent from "./Drawer";
+
+const useStyles = makeStyles((theme) => ({
+  darkColor:{
+    color: theme.palette.secondary.main
+  },
+  link: {
+    textDecoration: "none",
+    color: "white",
+    fontSize: "20px",
+    marginLeft: theme.spacing(20),
+    "&:hover": {
+      color: "yellow",
+      borderBottom: "1px solid white",
+    },
+  },
+  space:{
+  justifyContent: 'space-between'
+  }
+}));
+
+function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.space} >
+        <Typography variant="h1" className={classes.darkColor}>welcome</Typography>
+            <DrawerComponent />
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default NavBar;
