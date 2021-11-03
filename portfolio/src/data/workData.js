77814d0f451fb798c61d33a6d5bb8b049ba27e8e
Janet Iqal <Janet@MacBook-Air-2.local>
2021-11-03 15:26:54 -0500

    fixed err in data file

--- portfolio/src/data/workData.js
+++ portfolio/src/data/workData.js
@@ -3,14 +3,14 @@ const work=[
     title:'Nanny Poppins',
     githubRepo:'https://github.com/janetiqal/nanny-poppins',
     photo:'',
-    deployedApp:`https://nanny-poppins.herokuapp.com/`
+    deployedApp:`https://nanny-poppins.herokuapp.com/`,
     description:`An app where parents can search for nanny's that would be the perfect fit for their family, and an app where Nanny's can go to look for employment by creating profiles that showcase their credentials.`
 },
    {
     title:'Loc-Ale',
     githubRepo:'https://github.com/janetiqal/loc-ale',
     photo:'',
-    deployedApp:`https://br-through.github.io/loc-ale/`
+    deployedApp:`https://br-through.github.io/loc-ale/`,
     description:`Loc-Ale is a tool for finding breweries when a user searches for a city (google maps), it includes national news on breweries across the U.S and gives users local brewery information and classifications.`
 },
    
@@ -25,7 +25,7 @@ const work=[
     title:'Note Taker App',
     githubRepo:'https://github.com/janetiqal/Note-Taker-App',
     photo:'',
-    deployedApp:`https://note-taker-appji.herokuapp.com/`
+    deployedApp:`https://note-taker-appji.herokuapp.com/`,
     description:`This app has a front-end and a back-end that uses Express.js that allows users to create, save, and delete notes they've made throughout their day. This app is deployed on Heroku.`
 },   
 {
@@ -39,21 +39,21 @@ const work=[
     title:'Password Generator',
     githubRepo:'https://github.com/janetiqal/Password-Generator',
     photo:'',
-    deployedApp:`https://janetiqal.github.io/Password-Generator/`
+    deployedApp:`https://janetiqal.github.io/Password-Generator/`,
     description:`Password Generator app involves JavaScript where a user is prompted to answer a few questions with what kind of characters they would like to include in a randomly generated password. User is then presented with that password.`
 },  
  {
     title:'Code Quiz',
     githubRepo:'https://github.com/janetiqal/Code-Quiz',
     photo:'',
-    deployedApp:`https://janetiqal.github.io/Code-Quiz/`
+    deployedApp:`https://janetiqal.github.io/Code-Quiz/`,
     description:``
 },
  {
     title:'Tech Blog',
     githubRepo:'https://github.com/janetiqal/Tech-Blog',
     photo:'',
-    deployedApp:`https://tech-blog-ji.herokuapp.com/`
+    deployedApp:`https://tech-blog-ji.herokuapp.com/`,
     description:`This is a CMS style blog that was built using the MVC framework. Tech-blog allows user to sign in or signup and view all posts, comment or view comments on a post, as well as create, edit and delete their own blog posts.`
 },
 
