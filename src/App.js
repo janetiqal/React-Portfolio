f376e572f55d5b34c88120fa52c1ee4e7ec04fd6
Janet Iqal <Janet@MacBook-Air-2.cable.rcn.com>
2021-11-07 20:10:15 -0600

    fixed routing paths

--- src/App.js
+++ src/App.js
@@ -19,10 +19,10 @@ function App() {
         <Router>
           <NavBar />
           <Switch>
-            <Route path="/" exact component={HomePage}/>
-            <Route path="/work" exact component={WorkPage}/>
-            <Route path="/resume" exact component={ResumePage}/>
-            <Route path="/contact" exact  component={ContactPage}/>
+            <Route path="/React-Portfolio/" exact component={HomePage}/>
+            <Route path="/React-Portfolio/work" exact component={WorkPage}/>
+            <Route path="/React-Portfolio/resume" exact component={ResumePage}/>
+            <Route path="//React-Portfoliocontact" exact  component={ContactPage}/>
           </Switch>
         </Router>
         <Footer />
